080c419aa77c0a4b8523f36351ec39a05db12a57
hb <hb@aroma-project.org>
2017-02-10 20:04:06 -0800

    CLEANUP: There's on 'batchtools' strategy, so don't test it
    NOTE: Now the package passed 'R CMD check --as-cran' with all OK.

--- tests/dotdotdot.R
+++ tests/dotdotdot.R
@@ -34,7 +34,7 @@ sum_fcns$D <- function(x, y) {
 }
 
 
-for (strategy in c("eager", "lazy", "multiprocess", "batchtools")) {
+for (strategy in c("eager", "lazy", "multiprocess", "batchtools_interactive", "batchtools_local")) {
   plan(strategy, substitute=FALSE)
 
   for (name in names(sum_fcns)) {
