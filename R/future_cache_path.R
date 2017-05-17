3914f2063dc8598607ebe6c16f85ea16319f2d48
hb <hb@aroma-project.org>
2017-05-16 20:19:17 -0700

    Added support for R_FUTURE_CACHE_PATH (internal for now)

--- R/future_cache_path.R
+++ R/future_cache_path.R
@@ -1,24 +1,23 @@
-#' @importFrom R.utils isDirectory mkdirs
-#' @importFrom utils sessionInfo
+#' @importFrom utils file_test sessionInfo
 future_cache_path <- local({
   ## The path used for this session
   path <- NULL
 
-  function(root_path = ".future", absolute = TRUE, create = TRUE) {
+  function(root_path = Sys.getenv("R_FUTURE_CACHE_PATH", ".future"), absolute = TRUE, create = TRUE) {
     if (is.null(path)) {
       id <- basename(tempdir())
       id <- gsub("Rtmp", "", id, fixed = TRUE)
       timestamp <- format(Sys.time(), format = "%Y%m%d_%H%M%S")
       dir <- sprintf("%s-%s", timestamp, id)
       path_tmp <- file.path(root_path, dir)
-      if (create && !isDirectory(path_tmp)) {
-        mkdirs(path_tmp)
+      if (create && !file_test("-d", path_tmp)) {
+        dir.create(path_tmp, recursive = TRUE)
         pathname_tmp <- file.path(path_tmp, "sessioninfo.txt")
         writeLines(capture_output(print(sessionInfo())), con = pathname_tmp)
       }
       path <<- path_tmp
     }
-    if (absolute) path <- file.path(getwd(), path)
+    if (absolute) path <- normalizePath(path)
 
     path
   }
