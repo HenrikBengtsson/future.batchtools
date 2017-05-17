8ae913948bb70ea0ef7cf7ebafae1ff52bf5c72b
hb <hb@aroma-project.org>
2017-05-16 19:08:47 -0700

    TESTS: Added optional tests for batchjobs_* HPC schedulers listed in environment variable 'R_FUTURE_TESTS_STRATEGIES'

--- tests/incl/start,load-only.R
+++ tests/incl/start,load-only.R
@@ -9,6 +9,19 @@ future::plan(future.batchtools:::batchtools_local)
 
 fullTest <- (Sys.getenv("_R_CHECK_FULL_") != "")
 
+all_strategies <- function() {
+  strategies <- Sys.getenv("R_FUTURE_TESTS_STRATEGIES")
+  strategies <- unlist(strsplit(strategies, split = ","))
+  strategies <- gsub(" ", "", strategies)
+  strategies <- strategies[nzchar(strategies)]
+  strategies <- c(future:::supportedStrategies(), strategies)
+  unique(strategies)
+}
+
+test_strategy <- function(strategy) {
+  strategy %in% all_strategies()
+}
+
 attached_packages <- future.batchtools:::attached_packages
 await <- future.batchtools:::await
 delete <- future.batchtools:::delete
