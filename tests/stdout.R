7b53be350e2c0cf2eb2b7d0741cfb21f2461112f
Henrik Bengtsson <hb@aroma-project.org>
2018-12-24 00:44:23 -0800

    TESTS: Make assertion test agile to future changes where conditions incl. messages may be captured

--- tests/stdout.R
+++ tests/stdout.R
@@ -1,9 +1,5 @@
 source("incl/start.R")
 
-## Only validate 'stdout' output if future (>= 1.9.0)
-test_stdout <- ("stdout" %in% names(formals(future::Future)))
-if (!test_stdout) `%stdout%` <- function(x, ...) x
-
 message("*** Standard output ...")
 
 truth_rows <- utils::capture.output({
@@ -34,16 +30,13 @@ for (stdout in c(TRUE, FALSE, NA)) {
   str(r)
   stopifnot(value(f) == 42L)
 
-
-  if (test_stdout) {
-    if (is.na(stdout)) {
-      stopifnot(!"stdout" %in% names(r))
-    } else if (stdout) {
-      print(r)
-      stopifnot(identical(r$stdout, truth))
-    } else {
-      stopifnot(is.null(r$stdout))
-    }
+  if (is.na(stdout)) {
+    stopifnot(!"stdout" %in% names(r))
+  } else if (stdout) {
+    print(r)
+    stopifnot(identical(r$stdout, truth))
+  } else {
+    stopifnot(is.null(r$stdout))
   }
 
   v %<-% {
@@ -57,13 +50,11 @@ for (stdout in c(TRUE, FALSE, NA)) {
   out <- utils::capture.output(y <- v)
   stopifnot(y == 42L)
 
-  if (test_stdout) {
-    if (is.na(stdout) || !stdout) {
-      stopifnot(out == "")
-    } else {
-      print(out)
-      stopifnot(identical(out, truth_rows))
-    }
+  if (is.na(stdout) || !stdout) {
+    stopifnot(out == "")
+  } else {
+    print(out)
+    stopifnot(identical(out, truth_rows))
   }
 } ## for (stdout ...)
 
