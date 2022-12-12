932cdcba72ae7c1bfb5f34e35d32f7320ac2be8e
Henrik Bengtsson <hb@aroma-project.org>
2022-12-12 11:43:44 -0800

    Add a very large 'max.load' to SSH workers. It cannot be +Inf

--- R/BatchtoolsSSHFuture-class.R
+++ R/BatchtoolsSSHFuture-class.R
@@ -4,13 +4,25 @@
 #' @export
 BatchtoolsSSHFuture <- function(expr = NULL, substitute = TRUE, envir = parent.frame(), workers = availableCores(), ...) {
   if (substitute) expr <- substitute(expr)
+  assert_no_positional_args_but_first()
 
   if (is.null(workers)) workers <- availableCores()
   stopifnot(is.numeric(workers), length(workers) == 1L, !is.na(workers), is.finite(workers), workers >= 1L)
 
   dotdotdot <- list(...)
 
-  ssh_worker <- list(Worker$new("localhost", ncpus = 1L))
+  ## WORKAROUND: 'max.load' cannot be +Inf, because that'll lead to:
+  ##
+  ## Error in sample.int(x, size, replace, prob) : 
+  ##   too few positive probabilities
+  ##
+  ## in the submitJob() function created by makeClusterFunctionsSSH().
+  ## /HB 2022-12-12
+  ssh_worker <- list(Worker$new(
+    "localhost",
+    ncpus = 1L,
+    max.load = .Machine$double.xmax  ## +Inf fails
+  ))
 
   keep <- which(names(dotdotdot) %in% names(formals(makeClusterFunctionsSSH)))
   args <- c(list(workers = ssh_worker), dotdotdot[keep])
